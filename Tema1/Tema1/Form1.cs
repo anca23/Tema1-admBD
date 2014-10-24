14e3fd4b30e1cd723ef0fcf8709f12e41ae903e2
anca23 <anca.preda@ymail.com>
2014-10-24 21:13:52 +0300

    are layerele

--- Tema1/Tema1/Form1.cs
+++ Tema1/Tema1/Form1.cs
@@ -10,7 +10,7 @@ using System.Windows.Forms;
 
 namespace Tema1
 {
-    public partial class Form1 : Form
+    public partial class Form1 : Form,IPresentationLayer
     {
         public Form1()
         {
